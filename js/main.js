6bea30eee46ae1b0664300a4277408f0e918dea5
bruno lima <bru_jefferson@hotmail.com>
2025-07-12 15:39:03 -0300

    melhorando a logica e add cores no h2

--- js/main.js
+++ js/main.js
@@ -18,49 +18,47 @@ form.addEventListener("submit", (e) => {
 
     if (valorImc < 18.5) {
       resultado.innerHTML = `
-      <h2>Seu IMC é: ${valorImc}</h2>
+      <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
       <h3>Abaixo do Normal</h3>
       <p>Procure um médico. Algumas pessoas têm um baixo peso por características do seu organismo e tudo bem. Outras podem estar enfrentando problemas, como a desnutrição. É preciso saber qual é o caso</p>`;
       return;
     } else if (valorImc < 24.9) {
       resultado.innerHTML = `
-       <h2>Seu IMC é: ${valorImc}</h2>
+       <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
        <h3>Normal</h3>
        <p>Que bom que você está com o peso normal! E o melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação equilibrada.</p>`;
       return;
     } else if (valorImc < 29.9) {
     resultado.innerHTML = `
-       <h2>Seu IMC é: ${valorImc}</h2>
+       <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
        <h3>Sobrepeso</h3>
        <p>Ele é, na verdade, uma pré-obesidade e muitas pessoas nessa faixa já apresentam doenças associadas, como diabetes e hipertensão. Importante rever hábitos e buscar ajuda antes de, por uma série de fatores, entrar na faixa da obesidade pra valer.</p>`
        return;
     } else if (valorImc < 34.9) {
       resultado.innerHTML = `
-       <h2>Seu IMC é: ${valorImc}</h2>
+       <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
        <h3>Obesidade Grau I</h3>
        <p>Sinal de alerta! Chegou na hora de se cuidar, mesmo que seus exames sejam normais. Vamos dar início a mudanças hoje! Cuide de sua alimentação. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista.</p>`
        return;
     } else if (valorImc < 39.9) {
       resultado.innerHTML = `
-       <h2>Seu IMC é: ${valorImc}</h2>
+       <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
        <h3>Obesidade Grau II</h3>
        <p>Mesmo que seus exames aparentem estar normais, é hora de se cuidar, iniciando mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde.</p>`
        resultado.style.color="blue"
        return;
     } else {
       resultado.innerHTML = `
-       <h2>Seu IMC é: ${valorImc}</h2>
+       <h2 class="color-h2-valorImc">Seu IMC é: <span class="color-valorImc">${valorImc}</span></h2>
        <h3>Obesidade Grau III</h3>
        <p>Aqui o sinal é vermelho, com forte probabilidade de já existirem doenças muito graves associadas. O tratamento deve ser ainda mais urgente.</p>`
        return;
     } //fim do if secundário
   } else {
-    resultado.innerText = `por favor, insira valores válidos`;
+    resultado.innerHTML = `por favor, insira valores válidos`;
     resultado.style.color = "red";
     return;
   } //fim do if principal
 });
 
-/*  resultado.innerText= `Preencha Todos Os Campos Corretamente.`
-        resultado.style.color="red";
-        return; */
+
